05e6a600f56016ad89f8207609835de20223c185
RT-g <sao.brave.htam@gmail.com>
2021-02-13 16:40:34 +0900

    update laser.py and gather gas-species values to value.csv for ref

--- takeda/experiments/Laser_power/laser.py
+++ takeda/experiments/Laser_power/laser.py
@@ -1,5 +1,8 @@
 from math import exp
+import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import ticker, gridspec
 
 dx = 0.01e-3  # m Segment length
 dr = 0.005e-3  # m Segment length  # dr の分解能が大事っぽい
@@ -10,17 +13,7 @@ nt = 5000  # Number of time segments
 ng = 40  # 40  # Graphの分割数を決める値
 CFL = 0.7  # クーラン数 1以下にする 0.5~0.99くらいで，小さくしすぎると進みが遅い
 eta = 0.1  # 加熱効率
-select_gas = 'air'
 
-helium_a = 0.004519362
-helium_b = 1.10603162
-argon_a = 1.18
-argon_b = 0.21
-air_a = 0.354446826
-air_b = 0.379696856
-m_air = 28.966
-m_argon = 40.0
-m_helium = 4.0
 A_G = 0.224517656
 B_G = 0.77548
 sigma_G1 = 0.84473  # mm
@@ -35,31 +28,21 @@ M2_T = 21
 W_G0 = 1.7  # 単位はmm
 W_T0 = 2.0
 R_peak = 2.13
-
+hr = 20  # Heating region
 R_0 = 8314.0
 
-if select_gas == 'helium':
-    m = m_helium
-    gamma_gas = 1.67  # 単原子分子
-    eta_trans = 0.54
-    a = helium_a
-    b = helium_b
-    hr = 20  # Heating region
-elif select_gas == 'argon':
-    m = m_argon
-    gamma_gas = 1.67  # 単原子分子
-    eta_trans = 0.42
-    a = argon_a
-    b = argon_b
-    hr = 20  # Heating region
-elif select_gas == 'air':
-    m = m_air
-    gamma_gas = 1.4  # 2原子分子
-    eta_trans = 0.48
-    a = air_a
-    b = air_b
-    hr = 20  # Heating region
-
+# 気体種固有の値
+select_gas = 'air'
+gas_dict = pd.read_csv('../../data/value.csv', header=0, index_col=0).to_dict(orient='index')[select_gas]
+m = gas_dict['m']
+gamma_gas = gas_dict['gamma']
+eta_trans = gas_dict['eta_trans']
+a = gas_dict['a']
+b = gas_dict['b']
+a_low = gas_dict['a_low']
+b_low = gas_dict['b_low']
+ss = gas_dict['speed_of_sound']
+rho = gas_dict['rho']
 R = R_0 / m
 
 u_ionz0 = a * 524.4 ** b * 1.2 * 1e3  # m/s
@@ -79,6 +62,7 @@ umax_r = u_ionz0
 
 t_list = []
 uionz_list = []
+uionz_low_list = []
 p_list = []
 s_list = []
 x_list = []
@@ -104,10 +88,17 @@ for n1 in range(nt):
     W_T = W_T0 * 1e-3  # m
     # 波頭の値
     S_laser0 = R_peak * Power_laser / 4 / W_G / W_T * 1e-3  # GW/m ** 2 波頭のレーザー強度
-    u_ionz = a * (S_laser0) ** b * 1e3  # m/s 波頭の速度
+    u_ionz_line1 = a * (S_laser0 * 1e9 / rho / (ss**3)) ** b * ss  # m/s 波頭の速度
+    u_ionz_line3 = a_low * (S_laser0 * 1e9 / rho / (ss**3)) ** b_low * ss  # m/s 波頭の速度
+    if u_ionz_line1 > u_ionz_line3:
+        u_ionz = u_ionz_line1
+    else:
+        u_ionz = u_ionz_line3
+    # u_ionz = a * S_laser0 ** b * 1e3  # m/s 無次元化していない波頭の速度
     x_laser0 = x_laser0 + u_ionz * dt  # m 電離波面の波頭進展位置=Laserによって加熱される最初の位置
     x_laser = x_laser0  # m 横方向の加熱位置を決めるための処理。初期値はその時刻における波頭位置とする
     uionz_list.append(u_ionz * 1e-3)
+    # uionz_low_list.append(u_ionz_low * 1e-3)
     s_list.append(S_laser0)
     x_list.append(x_laser0 * 1e3)
 
@@ -147,25 +138,41 @@ for n1 in range(nt):
 #                 w = 0
 
 #figure()でグラフを表示する領域をつくり，figというオブジェクトにする．
-fig = plt.figure()
+fig = plt.figure(figsize=(10,8))
 
 #add_subplot()でグラフを描画する領域を追加する．引数は行，列，場所
-ax1 = fig.add_subplot(4, 1, 1)
-ax2 = fig.add_subplot(4, 1, 2)
-ax3 = fig.add_subplot(4, 1, 3)
-ax4 = fig.add_subplot(4, 1, 4)
+spec = gridspec.GridSpec(ncols=2, nrows=1, width_ratios=[1,4])
+ax1 = fig.add_subplot(4, 5, 1)
+ax2 = fig.add_subplot(4, 5, 6)
+ax3 = fig.add_subplot(4, 5, 11)
+ax4 = fig.add_subplot(4, 5, 16)
+ax5 = fig.add_subplot(spec[1])
 
-c1,c2,c3,c4 = "blue","green","red","black"      # 各プロットの色
-l1,l2,l3,l4 = "power","intensity","velocity","x"   # 各ラベル
+c1, c2, c3, c4 = "blue", "green", "red", "black"  # 各プロットの色
+l1, l2, l3, l4, l5, l6 = "power", "intensity", "velocity", "x", 'line1', 'line3'  # 各ラベル
 
 ax1.plot(t_list, p_list, color=c1, label=l1)
 ax2.plot(t_list, s_list, color=c2, label=l2)
 ax3.plot(t_list, uionz_list, color=c3, label=l3)
 ax4.plot(t_list, x_list, color=c4, label=l4)
-ax1.legend(loc = 'upper right') #凡例
-ax2.legend(loc = 'upper right') #凡例
-ax3.legend(loc = 'upper right') #凡例
-ax4.legend(loc = 'upper right') #凡例
+ax5.plot(s_list, uionz_list, color=c1, label=l5)
+# ax5.plot(s_list, uionz_low_list, color=c2, label=l6)
+ax1.legend(loc='upper right')  # 凡例
+ax2.legend(loc='upper right')  # 凡例
+ax3.legend(loc='upper right')  # 凡例
+ax4.legend(loc='upper right')  # 凡例
+ax5.legend(loc='upper right')  # 凡例
+
+ax1.vlines([2.3], 0, 1000, 'yellow', linestyles='dashed')
+ax2.vlines([2.3], 0, 1000, 'yellow', linestyles='dashed')
+ax3.vlines([2.3], 0, 1000, 'yellow', linestyles='dashed')
+ax4.vlines([2.3], 0, 1000, 'yellow', linestyles='dashed')
+ax5.set_xscale('log')
+ax5.get_xaxis().set_major_formatter(ticker.ScalarFormatter())
+ax5.set_yscale('log')
+ax5.get_yaxis().set_major_formatter(ticker.ScalarFormatter())
+ax5.vlines([500], 0.1, 10, 'red', linestyles='dashed')
+
 ax1.set_xlim(0, 5)
 ax1.set_ylim(0, 25)
 ax2.set_xlim(0, 5)
@@ -174,5 +181,8 @@ ax3.set_xlim(0, 5)
 ax3.set_ylim(0, 8)
 ax4.set_xlim(0, 5)
 ax4.set_ylim(0, 16)
-fig.tight_layout()  # レイアウトの設定
+ax5.set_xlim(20, 2000)
+ax5.set_ylim(0.3, 10)
+# fig.tight_layout()  # レイアウトの設定
+plt.savefig('laser.png')
 plt.show()
