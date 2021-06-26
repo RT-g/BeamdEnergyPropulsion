aa56759d52182e44f931d0047ee15c31c0ff5dda
RT-g <sao.brave.htam@gmail.com>
2021-06-26 20:32:01 +0900

    dir name yyMMddHH -> yyMMddHHmm

--- takeda/experiments/2D_Axisymmetrical/CFD_normalized_2D_Axisymmetric.m
+++ takeda/experiments/2D_Axisymmetrical/CFD_normalized_2D_Axisymmetric.m
@@ -6,7 +6,7 @@ clear, close all
 % clearvars
 
 % Movie
-base_dir = fullfile(pwd, string(datetime(now, 'ConvertFrom', 'datenum', 'Format', 'yyMMddHH')));
+base_dir = fullfile(pwd, string(datetime(now, 'ConvertFrom', 'datenum', 'Format', 'yyMMddHHmm')));
 mkdir(base_dir, 'Pressure');
 mkdir(base_dir, 'Temperature');
 mkdir(base_dir, 'U');
@@ -27,7 +27,7 @@ dx = 0.01e-3; % m Segment length, xi(ξ) direction
 dh = 0.01e-3; % m Segment length, eta(η) direction
 
 nx = 250; % Number of position segments, xは進展方向
-nh = 180; %2.5e-3/dh; %全長2.5 mmになるように設定 
+nh = 180; %2.5e-3/dh; %全長2.5 mmになるように設定
 nt = 2000; % Number of time segments
 ng = 40;%40; % Graphの分割数を決める値
 CFL = 0.7; % クーラン数 1以下にする 0.5~0.99くらいで，小さくしすぎると進みが遅い
@@ -119,7 +119,7 @@ Q3_cal = zeros(nx,nh);
 Q4_cal = ones(nx,nh).*P/(gamma-1);
 
 % diff vector of states
-dQ_1 = zeros(nx,nh); 
+dQ_1 = zeros(nx,nh);
 dQ_2 = zeros(nx,nh);
 dQ_3 = zeros(nx,nh);
 dQ_4 = zeros(nx,nh);
@@ -129,27 +129,27 @@ rhs2 = zeros(nx,nh);
 rhs3 = zeros(nx,nh);
 rhs4 = zeros(nx,nh);
 
-dQ1_1 = zeros(nx,nh); 
+dQ1_1 = zeros(nx,nh);
 dQ1_2 = zeros(nx,nh);
 dQ1_3 = zeros(nx,nh);
 dQ1_4 = zeros(nx,nh);
 
-dQ2_1 = zeros(nx,nh); 
+dQ2_1 = zeros(nx,nh);
 dQ2_2 = zeros(nx,nh);
 dQ2_3 = zeros(nx,nh);
 dQ2_4 = zeros(nx,nh);
 
-dQ3_1 = zeros(nx,nh); 
+dQ3_1 = zeros(nx,nh);
 dQ3_2 = zeros(nx,nh);
 dQ3_3 = zeros(nx,nh);
 dQ3_4 = zeros(nx,nh);
 
-dQ4_1 = zeros(nx,nh); 
+dQ4_1 = zeros(nx,nh);
 dQ4_2 = zeros(nx,nh);
 dQ4_3 = zeros(nx,nh);
 dQ4_4 = zeros(nx,nh);
 
-dQ5_1 = zeros(nx,nh); 
+dQ5_1 = zeros(nx,nh);
 dQ5_2 = zeros(nx,nh);
 dQ5_3 = zeros(nx,nh);
 dQ5_4 = zeros(nx,nh);
@@ -214,26 +214,26 @@ for n1 = 1:nt
     elseif (t*1e6<0.125)
         CFL=0.8;
     else
-        CFL=0.9; 
+        CFL=0.9;
     end
-    
+
     dt = CFL * min(dx/umax,  dh/vmax); %s, 計算位置>波面位置となるようにdtを決定
-    
+
     t = t+dt; %s
     t_list(n1,1) = t * 1e6; %us
 
     % レーザー強度の時間減衰(10J) 単位はMW
-    Power_laser = 1; 
-    % Power_laser = 8.15*exp(-0.866*t*1e6); 
+    Power_laser = 1;
+    % Power_laser = 8.15*exp(-0.866*t*1e6);
     % 正確に再現すると温度などが高く出すぎる
 %     if (t*1e6<0.085)
 %         Power_laser = 287.9222823*t*1e6+0.0005175756469;
 %     elseif (t*1e6<0.125)
 %         Power_laser =-476.3277981*t*1e6+66.1043297;
 %     else
-%         Power_laser = 8.15*exp(-0.866*t*1e6); 
-%     end  
-    
+%         Power_laser = 8.15*exp(-0.866*t*1e6);
+%     end
+
     % 進展方向のレーザー強度の変化を考慮しない場合のビーム半径
     W_G = W_G0*1e-3; %m
     W_T = W_T0*1e-3; %m
@@ -250,7 +250,7 @@ for n1 = 1:nt
     x_laser0 = x_laser0 + u_ionz0 * dt; %m Ionized wave front
 
     % η方向の計算
-    for n3 = 1:nh        
+    for n3 = 1:nh
         h = n3*dh; %m, general coordinate
 
         % 速度の調整
@@ -308,15 +308,15 @@ for n1 = 1:nt
     Q3_cal = Q3 + dQ_3;
     Q4_cal = Q4 + dQ_4;
 
-    % Boundary Conditions    
+    % Boundary Conditions
     % Left(回転軸)
     for i = 2:nx-1
-        Q1_cal(i,1) = Q1_cal(i,2); 
-        Q2_cal(i,1) = Q2_cal(i,2); 
+        Q1_cal(i,1) = Q1_cal(i,2);
+        Q2_cal(i,1) = Q2_cal(i,2);
         Q3_cal(i,1) = 0; %中心ではur=0のはず
         Q4_cal(i,1) = Q4_cal(i,2);
     end
-    
+
     % Right(流出)
     for i = 2:nx-1
         Q1_cal(i,nh) = Q1_cal(i,nh-1);
@@ -324,7 +324,7 @@ for n1 = 1:nt
         Q3_cal(i,nh) = Q3_cal(i,nh-1);
         Q4_cal(i,nh) = Q4_cal(i,nh-1);
     end
-    
+
     % Top(光軸方向)流出
     for i = 2:nh-1
         Q1_cal(nx,i) = Q1_cal(nx-1,i);
@@ -332,27 +332,27 @@ for n1 = 1:nt
         Q3_cal(nx,i) = Q3_cal(nx-1,i);
         Q4_cal(nx,i) = Q4_cal(nx-1,i);
     end
-    
+
     % Plate(アルミ板)
     Q1_cal(1,:) = Q1_cal(2,:);
     Q2_cal(1,:) = 0;
     Q3_cal(1,:) = 0;
     Q4_cal(1,:) = Q4_cal(2,:);
-    
+
     % Corners
     Q1_cal(nx,1) = (Q1_cal(nx,2) + Q1_cal(nx-1,1))/2;
     Q2_cal(nx,1) = (Q2_cal(nx,2) + Q2_cal(nx-1,1))/2;
     Q3_cal(nx,1) = (Q3_cal(nx,2) + Q3_cal(nx-1,1))/2;
     Q4_cal(nx,1) = (Q4_cal(nx,2) + Q4_cal(nx-1,1))/2;
-    
+
     Q1_cal(nx,nh) = (Q1_cal(nx-1,nh) + Q1_cal(nx,nh-1))/2;
     Q2_cal(nx,nh) = (Q2_cal(nx-1,nh) + Q2_cal(nx,nh-1))/2;
     Q3_cal(nx,nh) = (Q3_cal(nx-1,nh) + Q3_cal(nx,nh-1))/2;
     Q4_cal(nx,nh) = (Q4_cal(nx-1,nh) + Q4_cal(nx,nh-1))/2;
-    
+
 
     % 各行列の更新
-    
+
     Q1 = Q1_cal;
     Q2 = Q2_cal;
     Q3 = Q3_cal;
@@ -374,12 +374,12 @@ for n1 = 1:nt
     E2 = (rho.*u.*U + x_x.*P)./J;
     E3 = (rho.*U.*v + x_r.*P)./J;
     E4 = (e+P).*U./J;
-    
+
     F1 = rho.*V./J;
     F2 = (rho.*V.*u + h_x.*P)./J;
     F3 = (rho.*v.*V + h_r.*P)./J;
     F4 = (e+P).*V./J;
-    
+
     H1 = rho.*v./J;
     H2 = rho.*u.*v./J;
     H3 = rho.*v.^2./J;
@@ -399,17 +399,17 @@ for n1 = 1:nt
             g22 = x_h^2 + r_h^2;
             g12 = x_xi*x_h + r_xi*r_h;
             G0 = sqrt(g11/g22);
-            
+
             % ξ方向の計算
             % セル間の平均値の計算
             % Expressing rho, u, v and H in the basis of w (Roe's paper)
             D_x = sqrt(rho(n2+1,n3)/rho(n2,n3));
-            u_av_x = (u(n2,n3)+D_x*u(n2+1,n3))/(1+D_x); 
-            v_av_x = (v(n2,n3)+D_x*v(n2+1,n3))/(1+D_x); 
-            H_av_x = (H(n2,n3)+D_x*H(n2+1,n3))/(1+D_x); 
+            u_av_x = (u(n2,n3)+D_x*u(n2+1,n3))/(1+D_x);
+            v_av_x = (v(n2,n3)+D_x*v(n2+1,n3))/(1+D_x);
+            H_av_x = (H(n2,n3)+D_x*H(n2+1,n3))/(1+D_x);
             a_av_x = sqrt((gamma-1)*(H_av_x-1/2*(u_av_x.^2 + v_av_x.^2))); %Square root of the derivative of P br rho, used in the eigenvalues of the Jacobian matrix
 
-            % ヤコビアン行列の固有値計算 
+            % ヤコビアン行列の固有値計算
             U_av_x = u_av_x*x_x(n2,n3) + v_av_x*x_r(n2,n3);
             lambda_x(1) = U_av_x;
             lambda_x(2) = U_av_x;
@@ -473,17 +473,17 @@ for n1 = 1:nt
             E2_half(n2,n3) = 1/2*(E2(n2+1,n3) + E2(n2,n3)) - 1/2*(dE3(2)-dE2(2)) - (1-phi)/4*dE1(2) - (1+phi)/4*dE2(2) + (1+phi)/4*dE3(2) + (1-phi)/4*dE4(2);
             E3_half(n2,n3) = 1/2*(E3(n2+1,n3) + E3(n2,n3)) - 1/2*(dE3(3)-dE2(3)) - (1-phi)/4*dE1(3) - (1+phi)/4*dE2(3) + (1+phi)/4*dE3(3) + (1-phi)/4*dE4(3);
             E4_half(n2,n3) = 1/2*(E4(n2+1,n3) + E4(n2,n3)) - 1/2*(dE3(4)-dE2(4)) - (1-phi)/4*dE1(4) - (1+phi)/4*dE2(4) + (1+phi)/4*dE3(4) + (1-phi)/4*dE4(4);
-            
+
             % η方向の計算
             % セル間の平均値の計算
             % Expressing rho, u, v and H in the basis of w (Roe's paper)
             D_h = sqrt(rho(n2+1,n3)/rho(n2,n3));
-            u_av_h = (u(n2,n3)+D_h*u(n2+1,n3))/(1+D_h); 
-            v_av_h = (v(n2,n3)+D_h*v(n2+1,n3))/(1+D_h); 
-            H_av_h = (H(n2,n3)+D_h*H(n2+1,n3))/(1+D_h); 
+            u_av_h = (u(n2,n3)+D_h*u(n2+1,n3))/(1+D_h);
+            v_av_h = (v(n2,n3)+D_h*v(n2+1,n3))/(1+D_h);
+            H_av_h = (H(n2,n3)+D_h*H(n2+1,n3))/(1+D_h);
             a_av_h = sqrt((gamma-1)*(H_av_h-1/2*(u_av_h.^2 + v_av_h.^2))); %Square root of the derivative of P br rho, used in the eigenvalues of the Jacobian matrix
 
-            % ヤコビアン行列の固有値計算 
+            % ヤコビアン行列の固有値計算
             V_av_h = u_av_h*h_x(n2,n3) + v_av_h*h_r(n2,n3);
             lambda_h(1) = V_av_h;
             lambda_h(2) = V_av_h;
@@ -561,7 +561,7 @@ for n1 = 1:nt
                  [-v(n2,n3)^2 0 2*v(n2,n3) 0];...
                  [v(n2,n3)*(2*phi2-omega) -(gamma-1)*u(n2,n3)*v(n2,n3) omega-phi2-(gamma-1)*v(n2,n3)^2 gamma*v(n2,n3)]];
 
-            % dQの計算            
+            % dQの計算
             rhs1(n2,n3) = -dt/(1+theta_2)*((E1_half(n2,n3)-E1_half(n2-1,n3))/dx + (F1_half(n2,n3)-F1_half(n2,n3-1))/dh + H1(n2,n3)/r(n3)) + theta_2/(1+theta_2)*dQ_1(n2,n3);
             rhs2(n2,n3) = -dt/(1+theta_2)*((E2_half(n2,n3)-E2_half(n2-1,n3))/dx + (F2_half(n2,n3)-F2_half(n2,n3-1))/dh + H2(n2,n3)/r(n3)) + theta_2/(1+theta_2)*dQ_2(n2,n3);
             rhs3(n2,n3) = -dt/(1+theta_2)*((E3_half(n2,n3)-E3_half(n2-1,n3))/dx + (F3_half(n2,n3)-F3_half(n2,n3-1))/dh + H3(n2,n3)/r(n3)) + theta_2/(1+theta_2)*dQ_3(n2,n3);
@@ -608,7 +608,7 @@ for n1 = 1:nt
             dQ_4(n2,n3) = dQ(4);
         end
     end
-    
+
     if (mod(n1,ng) == 0)
         I = I+1;
         Per = n1/nt*100;
@@ -642,7 +642,7 @@ for n1 = 1:nt
         zlim([1 50]);
         frame = getframe(gcf);
         writeVideo(vp,frame);
-        
+
         % % make T_t movie
         % figure(f2);
         % view(135,45)
@@ -654,7 +654,7 @@ for n1 = 1:nt
         % zlim([1 5*1e4]);
         % frame = getframe(gcf);
         % writeVideo(vt,frame);
-        
+
         % make u_t movie
         figure(f3);
         view(135,45)
@@ -666,7 +666,7 @@ for n1 = 1:nt
         zlim([0 4]);
         frame = getframe(gcf);
         writeVideo(vu,frame);
-        
+
         % make v_t movie
         figure(f4);
         view(135,45)
@@ -678,7 +678,7 @@ for n1 = 1:nt
         zlim([0 3]);
         frame = getframe(gcf);
         writeVideo(vv,frame);
-        
+
         % make csv file
         pressure_filename = fullfile(base_dir, 'Pressure', append('Pressure_',num2str(I),'.csv'));
         temperature_filename = fullfile(base_dir, 'Temperature', append('Temperature_',num2str(I),'.csv'));
@@ -689,10 +689,10 @@ for n1 = 1:nt
         writematrix(u/1e3,u_filename)
         writematrix(v/1e3,v_filename)
         %M(I) = getframe(); %#ok<SAGROW>
-        
+
         %close
     end
-        
+
 end
 close(vp);
 close(vt);
@@ -705,5 +705,3 @@ writematrix(r_list,fullfile(base_dir,'r_scale.csv'))
 writematrix(Pplateau/1e5,fullfile(base_dir,'plateau_pressure_data.csv'))
 
 toc
-
-
