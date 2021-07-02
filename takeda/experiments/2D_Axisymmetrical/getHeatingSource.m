b89f4b1d0b744960b21a4707e8ae4d4bb7f5c4cc
RT-g <sao.brave.htam@gmail.com>
2021-07-02 16:47:58 +0900

    update heating func

--- takeda/experiments/2D_Axisymmetrical/getHeatingSource.m
+++ takeda/experiments/2D_Axisymmetrical/getHeatingSource.m
@@ -1,13 +1,9 @@
-function W = getHeatingSource(W, nx, dx x_laser0, l, S_laser0)
-
-    for n2 = 1:nx
-        x = n2*dx; %m, general coordinate
-        % 加熱領域をx_laser0よりも前にしてしまうと計算が壊れるので実際に考えられる値よりも少し進めたほうがいい?
-        if x > x_laser0-l && x < x_laser0 % ξが現実の長さを表しているわけではないので実際は少し補正が必要だが、ここでは無視
-            % W(n2,n3) = eta * S_laser(n3) / l * 1e9; %W/m3
-            W(n2,n3) = eta * S_laser0 / l * 1e9; %W/m3
-        else
-            W(n2,n3) = 0;å
-        end
+function w = getHeatingSource(x, cosine)
+    global Param
+    % 加熱領域をx_laser0よりも前にしてしまうと計算が壊れるので実際に考えられる値よりも少し進めたほうがいい?
+    if x > Param.LV.x_laser0-Param.LV.l && x < Param.LV.x_laser0 % ξが現実の長さを表しているわけではないので実際は少し補正が必要だが、ここでは無視
+        w = Param.rp.eta * Param.LV.S_laser0 * cosine / Param.LV.l * 1e9; %W/m3
+    else
+        w = 0;
     end
 end
