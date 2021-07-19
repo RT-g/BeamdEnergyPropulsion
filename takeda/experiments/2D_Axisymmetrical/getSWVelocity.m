a5699a9ebdfeeb24d13f69b3e52b913a68ba80e4
RT-g <sao.brave.htam@gmail.com>
2021-07-19 18:12:32 +0900

    change expression

--- takeda/experiments/2D_Axisymmetrical/getSWVelocity.m
+++ takeda/experiments/2D_Axisymmetrical/getSWVelocity.m
@@ -1,9 +1,9 @@
-function [u_ionz, intercept, B, r, cosine] = getSWVelocity(y)
+function [u_ionz, intercept, B, r, cosine] = getSWVelocity(rg)
     % 時刻に合わせて電離波面伝播速度を計算する。電離波面速度が早い方を採用するという仮定
     % TODO もう少し厳密な定義を考えたい
     global Param
-    [u_ionz_line1, intercept_line1, b_s_line1, r_line1, cosine_line1] = SWvelocity(Param.GC.slope, Param.GC.intercept, y);
-    [u_ionz_line3, intercept_line3, b_s_line3, r_line3, cosine_line3] = SWvelocity(Param.GC.slope_low, Param.GC.intercept_low, y);
+    [u_ionz_line1, intercept_line1, b_s_line1, r_line1, cosine_line1] = SWvelocity(Param.GC.slope, Param.GC.intercept, rg);
+    [u_ionz_line3, intercept_line3, b_s_line3, r_line3, cosine_line3] = SWvelocity(Param.GC.slope_low, Param.GC.intercept_low, rg);
 
     if (u_ionz_line1 > u_ionz_line3)
         u_ionz    = u_ionz_line1;
@@ -20,7 +20,7 @@ function [u_ionz, intercept, B, r, cosine] = getSWVelocity(y)
     end
 end
 
-function [u_ionz, intercept, B, r, cosine] = SWvelocity(slope, intercept, y)
+function [u_ionz, intercept, B, r, cosine] = SWvelocity(slope, intercept, rg)
     global Param
     A = Param.LV.a_s;
     B = intercept/(1-intercept);
@@ -30,9 +30,9 @@ function [u_ionz, intercept, B, r, cosine] = SWvelocity(slope, intercept, y)
 
     switch Param.hp.coodinate
         case 'general'
-            r = -(2/(3*A^2*B^2*y + sqrt(9*A^4*B^4*y^2 + 4*A^3*B^3)))^(1/3) + ((3*A^2*B^2*y + sqrt(9*A^4*B^4*y^2 + 4*A^3*B^3))/2)^(1/3)/A/B; %m, Cartesian coordinates
+            r = -(2/(3*A^2*B^2*rg + sqrt(9*A^4*B^4*rg^2 + 4*A^3*B^3)))^(1/3) + ((3*A^2*B^2*rg + sqrt(9*A^4*B^4*rg^2 + 4*A^3*B^3))/2)^(1/3)/A/B; %m, Cartesian coordinates
         case 'cartesian'
-            r = y;
+            r = rg;
     end
     G_r = Param.LV.A_G*exp(-2*(r*1e3/sqrt(2)/Param.LV.sigma_G1)^4)+Param.LV.B_G*exp(-2*(r*1e3/sqrt(2)/Param.LV.sigma_G2)^2);
     T_r = Param.LV.A_T*exp(-2*(r*1e3/sqrt(2)/Param.LV.sigma_T1)^4)+Param.LV.B_T*exp(-2*(r*1e3/sqrt(2)/Param.LV.sigma_T2)^2);
